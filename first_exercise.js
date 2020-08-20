1cd8fc0515887ac9b72a22239a098ba583957a55
WatCodeDatCode <arussell742@gmail.com>
2020-08-20 23:12:56 +0200

    Update variables

--- first_exercise.js
+++ first_exercise.js
@@ -1,5 +1,5 @@
-var readlineSync = require('readline-sync');
+let readlineSync = require('readline-sync');
  
 // Ask for user response.
-var name = readlineSync.question('May I have your name? ');
+let name = readlineSync.question('May I have your name? ');
 console.log('Hello ' + name + ', welcome to the Learn to Code course!'); //Prints user response to console
